e0f188210ad21c75cac6010956a7d00e8ea787e2
naomi <naomipacheco989@gmail.com>
2022-08-24 12:12:01 -0400

    predicciones1

---
+++ prediccion.js
@@ -0,0 +1,13 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }
+//el console.log imprime Naci en 1980
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+//el console.log imprime 1980
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+//el console.log imprime sumando numeros, el valor de num1, el valor de num2, valor 30
+  
