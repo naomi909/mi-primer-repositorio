a674a76ae23c9cc8ac476d03bc91dbc002e99ecc
naomi <naomipacheco989@gmail.com>
2022-08-24 13:05:54 -0400

    desafio

---
+++ desafio.js
@@ -0,0 +1,9 @@
+var alturaNiño = 45;
+function  muestraSiElNiñoPuedeSubirALaMontañaRusa(){
+    if(alturaNiño > 52){
+        console.log = ("¡Súbete, chico!");
+    }
+    else {
+            console.log = ("Lo siento, chico. Tal vez el próximo año");
+    }
+}
