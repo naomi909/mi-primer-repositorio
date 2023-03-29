ac0139410dfdf4722a42447c1c3c61a1d964cdf6
Naomi909 <naomipacheco989@gmail.com>
2023-03-29 10:23:13 -0300

    me gusta

--- JS/Me gusta/script.js
+++ JS/Me gusta/script.js
@@ -7,3 +7,20 @@ function sumar(){
     console.log(count);
 }
 
+var counter = 12;
+var counterElement = document.querySelector("#counter")
+
+function sumando(){
+    counter++;
+    counterElement.innerText = counter + "like(s)"
+    console.log(counter);
+}
+
+var counte = 12;
+var counteElement = document.querySelector("#counte")
+
+function sum(){
+    counte++;
+    counteElement.innerText = counte + "like(s)"
+    console.log(counte);
+}
